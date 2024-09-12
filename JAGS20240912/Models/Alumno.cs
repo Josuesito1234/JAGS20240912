de23eec1f531f039a6ddaa9cd686c8bf3377499e
Josuesito1234 <sosajosue2006@outlook.com>
2024-09-11 21:28:18 -0600

    Agregar archivos de proyecto.

---
+++ JAGS20240912/Models/Alumno.cs
@@ -0,0 +1,9 @@
+﻿namespace JAGS20240912.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
